2b72a4d894798eac5a68faabe98ba97dee4b5ff8
DEE <danv8605@gmail.com>
2023-12-29 13:52:17 -0600

    build workflow for production

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Daniel",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: import.meta.env.VITE_EMAIL_KEY,
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
